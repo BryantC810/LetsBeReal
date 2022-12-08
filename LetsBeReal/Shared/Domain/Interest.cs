91b012aaf0ce0ec83781eac53aa1208b39c0989e
CHIA SHAN YANG, BRYANT 2100554A <2100554A@student.tp.edu.sg>
2022-12-08 17:48:45 +0800

    Add project files.

---
+++ LetsBeReal/Shared/Domain/Interest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsBeReal.Shared.Domain
+{
+    class Interest
+    {
+        public string UserGenre { get; set; }
+        public int UserNumberOfGenre { get; set; }
+        public int UserID { get; set; }
+        public virtual User User { get; set; }
+    }
+}
---
+++ LetsBeReal/Shared/Domain/UserRank.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsBeReal.Shared.Domain
+{
+    class UserRank
+    {
+        public int RankNo { get; set; }
+        public int UserID { get; set; }
+        public int ReviewID { get; set; }
+        public virtual User User { get; set; }
+        public virtual Review Review { get; set; }
+    }
+}
